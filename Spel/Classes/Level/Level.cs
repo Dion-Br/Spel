59038df61cd5f008b4652fa621b220895518f26a
Dion Brovina <s129267@ap.be>
2022-12-21 22:08:04 +0100

    Level class edited

--- Spel/Classes/Level/Level.cs
+++ Spel/Classes/Level/Level.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +10,50 @@ namespace Spel.Classes.Level
 {
     public class Level
     {
-        public Background background { get; set; }
-        public Foreground foreground { get; set; }
+        private List<CollisionTiles> collisionTiles = new List<CollisionTiles>();
+        public List<CollisionTiles> CollisionTiles
+        {
+            get { return collisionTiles; }
+        }
 
-        public Level() { 
-        
+        private int width, height;
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public Level() { }
+
+        public void Generate(int[,] map, int size)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                for (int y = 0; y < map.GetLength(0); y++)
+                {
+                    int nr = map[y, x];
+
+                    if (nr > 0)
+                    {
+                        CollisionTiles.Add(new CollisionTiles(nr, new Rectangle(x * size, y * size, size, size)));
+                    }
+
+                    width = (x + 1) * size;
+                    height = (y + 1) * size;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(CollisionTiles tile in CollisionTiles)
+            {
+                tile.Draw(spriteBatch);
+            }
         }
     }
 }
---
+++ Spel/Classes/Level/Tiles.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel.Classes.Level
+{
+    // Bron: https://www.youtube.com/watch?v=PKlHcxFAEk0
+    public class Tiles
+    {
+        protected Texture2D texture;
+
+		private Rectangle rectangle;
+		public Rectangle Rectangle
+		{
+			get { return rectangle; }
+			set { rectangle = value; }
+		}
+
+		private static ContentManager content;
+		public static ContentManager Content
+		{
+			protected get { return content; }
+			set { content = value; }
+		}
+
+		public void Draw(SpriteBatch spritebatch)
+		{
+			spritebatch.Draw(texture, rectangle, Color.White);
+		}
+	}
+
+	public class CollisionTiles : Tiles
+	{
+		public CollisionTiles(int i, Rectangle newRectangle)
+		{
+			// Hier tile plaatsen
+			texture = Content.Load<Texture2D>("Tile" + i);
+			this.Rectangle = newRectangle;
+		}
+	}
+}
