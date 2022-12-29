dad5afb26439a413899685870decf67a3f4b1e45
Dion Brovina <s129267@ap.be>
2022-12-29 21:06:46 +0100

    Enemy added

---
+++ Spel/Classes/Character/Enemy.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Spel.Classes.Animations;
+using Spel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spel.Classes.Character
+{
+    class Enemy : IGameObject
+    {
+        private SpriteEffects se = SpriteEffects.None;
+        private int width = 32, height = 32, scale = 3;
+        private Texture2D enemyTexture;
+        private Vector2 position, speed;
+
+        Animation runAnimation, attackAnimation, deathAnimation;
+        AnimationManager animationManager;
+
+
+        public Enemy(Texture2D texture)
+        {
+            enemyTexture = texture;
+            position = new Vector2(500, 500);
+            speed = new Vector2(5, 5);
+
+            // Animaties ingeven.
+            MakeAnimations();
+
+            // Huidige animatie intialiseren.
+            animationManager = new AnimationManager();
+            SetCurrentAnimation(runAnimation);
+        }
+
+        void SetCurrentAnimation(Animation animation)
+        {
+            animationManager.CurrentAnimation = animation;
+        }
+
+        private void MakeAnimations()
+        {
+            // Set all the animation for the hero
+            deathAnimation = new Animation();
+            deathAnimation.AddSpriteRow(width, height, 0, 11);
+
+            runAnimation = new Animation();
+            runAnimation.AddSpriteRow(width, height, 1, 4);
+
+            attackAnimation = new Animation();
+            attackAnimation.AddSpriteRow(width, height, 2, 7);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(enemyTexture, position, animationManager.CurrentAnimation.CurrFrame.srcRectangle,
+                Color.White, 0f, new Vector2(0,0), scale, se, 0f);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            MoveX();
+
+            // Animatie updaten
+            animationManager.CurrentAnimation.Update(gameTime);
+        }
+
+        public void MoveX()
+        {
+            // Variabelen initialiseren
+            int widthBorder = 1400;
+            int rightBorder = widthBorder - width * scale;
+
+            position.X += speed.X;
+
+            // Hero mag niet uit de rechterkant van het scherm lopen
+            if (position.X > rightBorder)
+            {
+                speed.X *= -1;
+                se = SpriteEffects.FlipHorizontally;
+            }
+
+            // Hero mag niet uit de linkerkant van het scherm lopen
+            if (position.X < 0)
+            {
+                speed.X *= -1;
+                se = SpriteEffects.None;
+            }
+        }
+    }
+}
--- Spel/Classes/GameStates/Playing.cs
+++ Spel/Classes/GameStates/Playing.cs
@@ -18,51 +18,92 @@ namespace Spel.Classes.GameStates
     internal class Playing : GameState
     {
         // Variabelen initialiseren
-        private Texture2D _backgroundTexture, _heroTexture;
+        private Texture2D _backgroundTexture, _heroTexture, _enemyTexture;
 
         private Hero hero;
+        private Enemy enemy1;
         private Background background;
         private KeyboardReader KBReader;
-        public Map map;
+        private Map level1, level2, currentLevel; 
 
         public Playing(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
             KBReader = new KeyboardReader();
-            map = new Map();
 
+            _enemyTexture = _content.Load<Texture2D>("enemy");
             _heroTexture = _content.Load<Texture2D>("sprite");
             _backgroundTexture = _content.Load<Texture2D>("background");
 
             Tiles.Content = content;
             hero = new Hero(_heroTexture, KBReader);
+            enemy1 = new Enemy(_enemyTexture);
             background = new Background(_backgroundTexture);
 
-            map.Generate(new int[,]
-            {
-                {0,0,0,0,0,0,0,0,0,0,0 },
-                {0,0,0,1,1,1,0,1,1,1,1 },
-                {0,1,0,0,0,0,0,0,0,0,0 },
-                {0,0,1,1,1,0,0,0,1,0,0 },
-                {0,0,0,0,0,0,0,1,1,0,0 },
-                {1,1,1,1,1,1,1,1,1,1,1 },
-                {0,0,0,0,0,0,0,0,0,0,0 }
-            }, 128);
+            GenerateLevels();
+            currentLevel = level2;
         }
-        
+
+        private void GenerateLevels()
+        {
+            level1 = new Map();
+            level2 = new Map();
+
+            level1.Generate(new int[,]
+            {
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0},
+                {0,0,0,0,1,1,0,0,0,0,1,0,0,0,1,1,1,0,0,0,0,0},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            }, 64);
+
+            level2.Generate(new int[,]
+            {
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0},
+                {0,1,0,1,1,1,1,1,1,0,1,0,0,0,1,1,1,0,0,0,0,0},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            }, 64);
+        }
+
         public override void Update(GameTime gameTime)
         {
             hero.Update(gameTime);
-            foreach(CollisionTiles tile in map.CollisionTiles)
+            enemy1.Update(gameTime);
+            foreach(CollisionTiles tile in currentLevel.CollisionTiles)
             {
-                hero.Collision(tile.Rectangle, map.Width, map.Height);
+                hero.Collision(tile.Rectangle, currentLevel.Width, currentLevel.Height);
             }
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             background.Draw(spriteBatch);
-            map.Draw(spriteBatch);
+            currentLevel.Draw(spriteBatch);
+            enemy1.Draw(spriteBatch);
             hero.Draw(spriteBatch);
+        }
+
+        public void NextLevel()
+        {
+            currentLevel = level2;
         }
     }
 }
